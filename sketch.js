31ebf25dccd1a55dc8eda3df5b59fee2cd872fc0
Noel Alejandro Gutierrez <100980921+noelalejandro@users.noreply.github.com>
2023-04-26 18:02:50 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,178 +1,99 @@
+// Ball class
 class Ball {
-  constructor(x, y, r) {
-    this.position = new p5.Vector(x, y);
-    this.velocity = p5.Vector.random2D();
-    this.velocity.mult(3);
-    this.r = r;
-    this.m = r * 0.1;
-  }
-  update() {
-    this.position.add(this.velocity);
-  }
-
-  checkBoundaryCollision() {
-    if (this.position.x > width - this.r) {
-      this.position.x = width - this.r;
-      this.velocity.x *= -1;
-    } else if (this.position.x < this.r) {
-      this.position.x = this.r;
-      this.velocity.x *= -1;
-    } else if (this.position.y > height - this.r) {
-      this.position.y = height - this.r;
-      this.velocity.y *= -1;
-    } else if (this.position.y < this.r) {
-      this.position.y = this.r;
-      this.velocity.y *= -1;
-    }
+  constructor() {
+    this.x = width/2;
+    this.y = 25;
+    this.dx = random(-20,20);
+    this.dy = 20;
+    this.bounciness = 0.75;
+    this.diameter = random(10, 30);
+    this.speed = 1;
   }
 
-  checkCollision(other) {
-    // Get distances between the balls components
-    let distanceVect = p5.Vector.sub(other.position, this.position);
-
-    // Calculate magnitude of the vector separating the balls
-    let distanceVectMag = distanceVect.mag();
-
-    // Minimum distance before they are touching
-    let minDistance = this.r + other.r;
-
-    if (distanceVectMag < minDistance) {
-      let distanceCorrection = (minDistance - distanceVectMag) / 2.0;
-      let d = distanceVect.copy();
-      let correctionVector = d.normalize().mult(distanceCorrection);
-      other.position.add(correctionVector);
-      this.position.sub(correctionVector);
-
-      // get angle of distanceVect
-      let theta = distanceVect.heading();
-      // precalculate trig values
-      let sine = sin(theta);
-      let cosine = cos(theta);
-
-      /* bTemp will hold rotated ball this.positions. You 
-       just need to worry about bTemp[1] this.position*/
-      let bTemp = [new p5.Vector(), new p5.Vector()];
-
-      /* this ball's this.position is relative to the other
-       so you can use the vector between them (bVect) as the 
-       reference point in the rotation expressions.
-       bTemp[0].this.position.x and bTemp[0].this.position.y will initialize
-       automatically to 0.0, which is what you want
-       since b[1] will rotate around b[0] */
-      bTemp[1].x = cosine * distanceVect.x + sine * distanceVect.y;
-      bTemp[1].y = cosine * distanceVect.y - sine * distanceVect.x;
-
-      // rotate Temporary velocities
-      let vTemp = [new p5.Vector(), new p5.Vector()];
-
-      vTemp[0].x = cosine * this.velocity.x + sine * this.velocity.y;
-      vTemp[0].y = cosine * this.velocity.y - sine * this.velocity.x;
-      vTemp[1].x = cosine * other.velocity.x + sine * other.velocity.y;
-      vTemp[1].y = cosine * other.velocity.y - sine * other.velocity.x;
-
-      /* Now that velocities are rotated, you can use 1D
-       conservation of momentum equations to calculate 
-       the final this.velocity along the x-axis. */
-      let vFinal = [new p5.Vector(), new p5.Vector()];
-
-      // final rotated this.velocity for b[0]
-      vFinal[0].x =
-        ((this.m - other.m) * vTemp[0].x + 2 * other.m * vTemp[1].x) /
-        (this.m + other.m);
-      vFinal[0].y = vTemp[0].y;
-
-      // final rotated this.velocity for b[0]
-      vFinal[1].x =
-        ((other.m - this.m) * vTemp[1].x + 2 * this.m * vTemp[0].x) /
-        (this.m + other.m);
-      vFinal[1].y = vTemp[1].y;
-
-      // hack to avoid clumping
-      bTemp[0].x += vFinal[0].x;
-      bTemp[1].x += vFinal[1].x;
-
-      /* Rotate ball this.positions and velocities back
-       Reverse signs in trig expressions to rotate 
-       in the opposite direction */
-      // rotate balls
-      let bFinal = [new p5.Vector(), new p5.Vector()];
-
-      bFinal[0].x = cosine * bTemp[0].x - sine * bTemp[0].y;
-      bFinal[0].y = cosine * bTemp[0].y + sine * bTemp[0].x;
-      bFinal[1].x = cosine * bTemp[1].x - sine * bTemp[1].y;
-      bFinal[1].y = cosine * bTemp[1].y + sine * bTemp[1].x;
-
-      // update balls to screen this.position
-      other.position.x = this.position.x + bFinal[1].x;
-      other.position.y = this.position.y + bFinal[1].y;
-
-      this.position.add(bFinal[0]);
-
-      // update velocities
-      this.velocity.x = cosine * vFinal[0].x - sine * vFinal[0].y;
-      this.velocity.y = cosine * vFinal[0].y + sine * vFinal[0].x;
-      other.velocity.x = cosine * vFinal[1].x - sine * vFinal[1].y;
-      other.velocity.y = cosine * vFinal[1].y + sine * vFinal[1].x;
-    }
+  move() {
+    this.x += this.dx;
+    this.y += this.dy;
+    this.dx += wind;
+    this.dy += gravity;
+    this.dx = this.dx * hold;
+    this.dy = this.dy * hold;
   }
 
   display() {
-    noStroke();
-    fill(204);
-    ellipse(this.position.x, this.position.y, this.r * 2, this.r * 2);
+    ellipse(this.x, this.y, this.diameter, this.diameter);
+  }
+  
+  checkBoundaryCollision() {
+    if (this.x > width - this.diameter/2) {
+      this.x = width - this.diameter/2;
+      this.dx *= -this.dx * bounciness;
+    } else if (this.x < this.diameter/2) {
+      this.x = this.diameter/2;
+      this.dx *= -this.dx * bounciness;
+    } else if (this.y > height - this.diameter/2) {
+      this.y = height - this.diameter/2;
+      this.dy *= -this.dy * bounciness;
+    } else if (this.y < this.diameter) {
+      this.y = this.diameter;
+      this.y *= -this.dy * bounciness;
+    }
   }
 }
 
-let balls = [new Ball(100, 400, 80), new Ball(700, 400, 80)];
-console.log(balls);
-}
+let balls = [];
 
-//======================================================================
-
-let osc, playing, freq, amp;
-
-// function setup() {
-//   for (let i = 0; i < 50; i++) {
-//     balls.push(new Ball());
-//   }
-  let cnv = createCanvas(710, 400);
-  cnv.mousePressed(playOscillator);
-  osc = new p5.Oscillator('sine');
+function setup() {
+  createCanvas(710, 400);
+  // Create objects
+  for (let i = 0; i < 50; i++) {
+    balls.push(new Ball());
+  }
 }
 
 function draw() {
-  background(51);
+  background(50, 89, 100);
   for (let i = 0; i < balls.length; i++) {
-    let b = balls[i];
-    b.update();
-    b.display();
-    b.checkBoundaryCollision();
-    balls[0].checkCollision(balls[1]);
-  }
-  freq = constrain(map(mouseX, 0, width, 100, 500), 100, 500);
-  amp = constrain(map(mouseY, height, 0, 0, 1), 0, 1);
-
-  text('tap to play', 20, 20);
-  text('freq: ' + freq, 20, 40);
-  text('amp: ' + amp, 20, 60);
-
-  if (playing) {
-    // smooth the transitions by 0.1 seconds
-    osc.freq(freq, 0.1);
-    osc.amp(amp, 0.1);
+    balls[i].move();
+    balls[i].display();
   }
 }
 
-function playOscillator() {
-  // starting an oscillator on a user gesture will enable audio
-  // in browsers that have a strict autoplay policy.
-  // See also: userStartAudio();
-  osc.start();
-  playing = true;
-}
+//=============================================================
 
-function mouseReleased() {
-  // ramp amplitude to 0 over 0.5 seconds
-  osc.amp(0, 0.5);
-  playing = false;
+let gravity = 0.9;
+let wind = 0.0;
+let hold = 1;
+
+function keyPressed()
+{
+//   if (key == ' ')
+//   {
+//     // new ball
+//     Ball b = bouncingBall();
+//     balls.add(b);
+//   }
+  
+  if (key == CODED) 
+  {
+    if (keyCode == UP) 
+    {
+      gravity = gravity - 0.25;
+    }
+    if (keyCode == DOWN)
+    {
+      gravity = gravity + 0.25;
+    }
+    if (keyCode == LEFT) 
+    {
+      wind = wind - 0.25;
+    }
+    if (keyCode == RIGHT)
+    {
+      wind = wind + 0.25;
+    }
+//     if (keyCode == ESC)
+//     {
+      // stop the program
+    }
+  }
 }
