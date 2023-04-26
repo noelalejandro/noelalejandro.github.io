c9a6c1520de22bfa5345ad8ced1c27ca0eb9dd36
Noel Alejandro Gutierrez <100980921+noelalejandro@users.noreply.github.com>
2023-04-26 15:12:30 -0500

    Update audi314.js

--- audi314.js
+++ audi314.js
@@ -1,34 +1,37 @@
-let coswave = [];
+let osc, playing, freq, amp;
 
 function setup() {
-  createCanvas(720, 360);
-  for (let i = 0; i < width; i++) {
-    let amount = map(i, 0, width, 0, PI);
-    coswave[i] = abs(cos(amount));
-  }
-  background(255);
-  noLoop();
+  let cnv = createCanvas(100, 100);
+  cnv.mousePressed(playOscillator);
+  osc = new p5.Oscillator('sine');
 }
 
 function draw() {
-  let y1 = 0;
-  let y2 = height / 3;
-  for (let i = 0; i < width; i += 3) {
-    stroke(coswave[i] * 255);
-    line(i, y1, i, y2);
-  }
+  background(220)
+  freq = constrain(map(mouseX, 0, width, 100, 500), 100, 500);
+  amp = constrain(map(mouseY, height, 0, 0, 1), 0, 1);
 
-  y1 = y2;
-  y2 = y1 + y1;
-  for (let i = 0; i < width; i += 3) {
-    stroke((coswave[i] * 255) / 4);
-    line(i, y1, i, y2);
-  }
+  text('tap to play', 20, 20);
+  text('freq: ' + freq, 20, 40);
+  text('amp: ' + amp, 20, 60);
 
-  y1 = y2;
-  y2 = height;
-  for (let i = 0; i < width; i += 3) {
-    stroke(255 - coswave[i] * 255);
-    line(i, y1, i, y2);
+  if (playing) {
+    // smooth the transitions by 0.1 seconds
+    osc.freq(freq, 0.1);
+    osc.amp(amp, 0.1);
   }
+}
+
+function playOscillator() {
+  // starting an oscillator on a user gesture will enable audio
+  // in browsers that have a strict autoplay policy.
+  // See also: userStartAudio();
+  osc.start();
+  playing = true;
+}
+
+function mouseReleased() {
+  // ramp amplitude to 0 over 0.5 seconds
+  osc.amp(0, 0.5);
+  playing = false;
 }
